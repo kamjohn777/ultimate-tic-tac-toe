a374dc094ae01262f163000819e4026e717b1b92
kamjohn777 <kamjohn79.kdj@gmail.com>
2023-10-27 15:00:38 -0500

    worked on the functions a bit

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const mainGameboard = document.querySelector('#main-gameboard');
+
+/* Current Player Variables */
 let currentPlayer = 'cross';
-let boardCells = [
-    "", "", "", "", "", "", "", "", ""
-]
+
+/* Board/Cells Variables */
+const mainGameboard = document.querySelector('#main-gameboard');
+let boardCells = [ "", "", "", "", "", "", "", "", ""];
 let activeSubBoards = [false, false, false, false, false, false, false, false, false];
 let lastMoveSubCell = null;
 
+/* Winning combos Variables */
+const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
+    [0, 4, 8], [2, 4, 6]              // Diagonals
+];
+
+ let infoDisplay = document.querySelector('#info')
+
+
+/* Creating Main Board Cells */
 function creatingGameBoard() {
     boardCells.forEach((_cell, index) => {
         const individualCell = document.createElement('div');
@@ -25,7 +38,7 @@ function creatingGameBoard() {
 
 creatingGameBoard();
 
-
+/* Creating SubBoard Cells */
 function subBoardsPerCell() {
     const cells = document.querySelectorAll('.cells'); // Select all cells
     cells.forEach((individualCell, sbBoard) => {
@@ -53,9 +66,60 @@ function subBoardsPerCell() {
 
 subBoardsPerCell();
 
+/* Checks winning sub board combos */
+function checkSubBoardWinner(subBoardIndex) {
+    const subBoard = document.getElementById('sub' + subBoardIndex);
+    const cells = subBoard.querySelectorAll('.sub-cell');
+    console.log(subBoard)
+    console.log(cells);
+    const winningCombos = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
+
+    winningCombos.forEach(array => {
+       let circleWins = array.every(cell => cells[cell].firstChild?.classList.contains('circle'));
+
+       if (circleWins) {
+            infoDisplay.textContent = 'Circle Wins!';
+            // subBoard.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+            cells.forEach(cell => cell.replaceWith(cell.cloneNode(true)));
+            return;
+       }
+    })
+    
+    
+    for (let combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (cells[a].querySelector('.circle') && cells[b].querySelector('.circle') && cells[c].querySelector('.circle')) {
+            return 'circle';
+        } else if (cells[a].querySelector('.cross') && cells[b].querySelector('.cross') && cells[c].querySelector('.cross')) {
+            return 'cross';
+        }
+    }
+    
+    return null;  // No winner yet
+}
+
+// 
+
+
 function handleSubCellClick(sbBoard, subCellIndex) {
     const subCell = document.getElementById('sub-cell' + subCellIndex + '-' + sbBoard);
     lastMoveSubCell = subCellIndex;
+
+    
+    // Check if there's a winner after the move
+    const winner = checkSubBoardWinner(sbBoard);
+    if (winner) {
+        const subBoardDiv = document.getElementById('sub' + sbBoard);
+        subBoardDiv.classList.add(winner + '-won'); // Adds either "circle-won" or "cross-won" to the sub-board
+        
+        // Optionally: Display a message if needed
+        const msgDisplay = document.getElementById('turn-msg');
+        msgDisplay.innerHTML = `Player ${winner} wins the sub-board ${sbBoard}`;
+    }
 
     if (subCell.querySelector('.circle') || subCell.querySelector('.cross')) {
         // The sub-cell is already occupied, so return early.
@@ -126,7 +190,7 @@ updateActiveSubBoard();
 
 function isSubBoardFull(subBoardIndex) {
     const subBoard = document.querySelectorAll('.sub')[subBoardIndex];
-    const cells = subBoard.querySelectorAll('.cell');
+    const cells = subBoard.querySelectorAll('.sub-cell');
     for (let cell of cells) {
         if (!cell.classList.contains('cross') && !cell.classList.contains('circle')) {
             return false;
@@ -143,3 +207,7 @@ function handleFullSubBoard() {
 
 
 // try to ue conditions to toggle between hovers in the css when its circle hover a certain color and vise versa
+
+
+//  do like if (subaboardCells === winning combos) {subboard.style.display = ".winningBoardDisplay"}
+// so in the line above we can try and use the style to set up the display if a player wins a board and update the css style later so these are all just fill ins rigth now 
